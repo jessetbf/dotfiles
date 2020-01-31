ba1daea67d151e2eccff075fe69774625f3a0737
Jesse <jesset@bestfriends.org>
2020-01-31 15:51:31 -0700

    remove unused programs

--- install/cask.sh
+++ install/cask.sh
@@ -24,27 +24,19 @@ apps=(
   iterm2
   java
   jumpcut
-  kindle
   mamp
-  macdown
   mucommander
   nextcloud
-  opera
   osxfuse
   p4merge
   sequel-pro
   slack
-  sourcetree
   synergy
   textual
   vagrant
-  vectr
   veracrypt
   virtualbox
   vlc
 )
 
 brew cask install "${apps[@]}"
-
-# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
